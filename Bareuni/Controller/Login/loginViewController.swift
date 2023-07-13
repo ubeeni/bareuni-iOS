20804c0b2a41c9e9404c963c8b8c184414b64f7d
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2023-07-14 00:13:52 +0900

    [Feat] Login 페이지 UI 만들기

--- Bareuni/Controller/Login/loginViewController.swift
+++ Bareuni/Controller/Login/loginViewController.swift
@@ -12,18 +12,44 @@ class loginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // uiview 설정
+        emailTextField.doViewSetting(paddingWidth: 28, cornerRadius: 12)
+        pwTextField.doViewSetting(paddingWidth: 28, cornerRadius: 12)
+        
+        loginSubmitBtn.layer.cornerRadius = 12
+        
+        findPWBtn.setTitleColor(UIColor(red: 0.459, green: 0.459, blue: 0.459, alpha: 1), for: .normal)
     }
     
 
-    /*
-    // MARK: - Navigation
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var loginSubmitBtn: UIButton!
+    
+    @IBOutlet weak var findPWBtn: UIButton!
+}
+
+extension UITextField{
+    func doViewSetting(paddingWidth: Float, cornerRadius: Float){
+        
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: CGFloat(paddingWidth), height: self.frame.height))
+        
+        self.leftView = paddingView
+        self.leftView?.layer.borderColor = UIColor(red: 0.837, green: 1, blue: 0.837, alpha: 1).cgColor
+        self.leftViewMode = ViewMode.always
+        
+        self.rightView = paddingView
+        self.rightView?.backgroundColor = .none
+        self.rightView?.layer.borderColor = .none
+        self.rightViewMode = ViewMode.always
+        
+        self.layer.cornerRadius = 12
+        self.layer.borderWidth = 1
+        
+        self.layer.borderColor = UIColor(red: 0.837, green: 0.837, blue: 0.837, alpha: 1).cgColor
+
+
     }
-    */
-
 }
