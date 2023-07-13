8fd9126dc30b45295d8209000e487b1d354ef73c
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2023-07-13 22:41:21 +0900

    [Feat] LaunchScreen 추가하기

---
+++ Bareuni/Controller/Login/loginSelectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  loginSelectionViewController.swift
+//  Bareuni
+//
+//  Created by 윤지성 on 2023/07/13.
+//
+
+import UIKit
+
+class loginSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
