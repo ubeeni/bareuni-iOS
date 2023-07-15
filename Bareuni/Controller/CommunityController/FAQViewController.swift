dda7686a2570520a239edcde2b8c93a3515775fe
wisdom <wisdom0405@kakao.com>
2023-07-15 22:31:51 +0900

    Style Community Home

---
+++ Bareuni/Controller/CommunityController/FAQViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FAQViewController.swift
+//  Bareuni
+//
+//  Created by 정슬기 on 2023/07/15.
+//
+
+import UIKit
+
+class FAQViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
