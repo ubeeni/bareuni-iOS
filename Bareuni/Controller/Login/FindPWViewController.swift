e6c04b20763f1be7b071c9dc7d036c171825180f
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2023-07-18 13:26:26 +0900

    [Feat] 비밀번호 찾기 화면 - 이메일 형식 검사 로직 추가

--- Bareuni/Controller/Login/FindPWViewController.swift
+++ Bareuni/Controller/Login/FindPWViewController.swift
@@ -15,6 +15,7 @@ class FindPWViewController: UIViewController {
         explainLb.numberOfLines = 2
         explainLb.text = "가입한 이메일 주소를 입력해주세요 \n비밀번호 재설정을 위한 이메일을 보내드리겠습니다 "
         findEmailTF.doViewSetting(paddingWidth: 28, cornerRadius: 12)
+        self.findEmailTF.addTarget(self, action: #selector(self.textFieldDidChange(_sender:)), for: .editingChanged)
         
         confirmBtn.backgroundColor = UIColor(red: 0.821, green: 0.821, blue: 0.821, alpha: 1)
         confirmBtn.layer.cornerRadius = 12
@@ -24,5 +25,27 @@ class FindPWViewController: UIViewController {
 
     @IBOutlet weak var explainLb: UILabel!
     @IBOutlet weak var findEmailTF: UITextField!
+    @IBOutlet weak var warningLb: UILabel!
     @IBOutlet weak var confirmBtn: UIButton!
+    
+    @objc func textFieldDidChange(_sender: Any) {
+        if(checkEmail(str: self.findEmailTF.text ?? "") == false){
+            explainLb.layer.borderColor = UIColor(red: 1, green: 0.314, blue: 0.314, alpha: 1).cgColor
+            
+            warningLb.text = "이메일을 확인해주세요."
+            warningLb.textColor = UIColor(red: 1, green: 0.314, blue: 0.314, alpha: 1)
+            confirmBtn.backgroundColor = UIColor(red: 0.821, green: 0.821, blue: 0.821, alpha: 1)
+        }
+        else{
+            findEmailTF.doViewSetting(paddingWidth: 28, cornerRadius: 12)
+            warningLb.text = "대소문자를 구분하여 입력해주세요."
+            warningLb.textColor = UIColor(red: 0.459, green: 0.459, blue: 0.459, alpha: 1)
+            confirmBtn.backgroundColor = UIColor(named: "BackgroundBlue")
+        }
+    }
+    
+    func checkEmail(str: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+        return  NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: str)
+    }
 }
