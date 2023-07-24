54d546120f406ae74b6754a1b97c05f6d8518320
김유빈 <yubin6894@gmail.com>
2023-07-24 17:23:51 +0900

    [Feat] 커뮤니티 화면 세팅

---
+++ Bareuni/Controller/Community/CommunityVC.swift
@@ -0,0 +1,22 @@
+//
+//  CommunityVC.swift
+//  Bareuni
+//
+//  Created by 김유빈 on 2023/07/24.
+//
+
+import UIKit
+import SwiftUI
+
+class CommunityVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    @IBSegueAction func addCommunityView(_ coder: NSCoder) -> UIViewController? {
+        return UIHostingController(coder: coder, rootView: CommunityMainView())
+    }
+}
---
+++ Bareuni/View/CommunityMainView.swift
@@ -0,0 +1,20 @@
+//
+//  CommunityMainView.swift
+//  Bareuni
+//
+//  Created by 김유빈 on 2023/07/24.
+//
+
+import SwiftUI
+
+struct CommunityMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CommunityMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        CommunityMainView()
+    }
+}
