1b00f1ab4c68b15ab4c186273d7e432e56edd14e
김유빈 <yubin6894@gmail.com>
2023-07-25 22:35:03 +0900

    [Feat] 커뮤니티 메인 UI 구현 완료

--- Bareuni/View/CommunityMainView.swift
+++ Bareuni/View/CommunityMainView.swift
@@ -7,7 +7,24 @@
 
 import SwiftUI
 
+struct Post: Identifiable {
+    let id = UUID()
+    let content: String
+    let modificationTime: String
+}
+
+
 struct CommunityMainView: View {
+    
+    let dummyPosts: [Post] = [
+        Post(content: "저는 치아교정한지 2년 3개월 된 사람입니다. 다름이 아니라 현재 이런 고민이 있는데요.", modificationTime: "12시간 전 (수정됨)"),
+        Post(content: "교정을 마친 지 3년째인데 혹시 저랑 같은 문제가 생기신 분 계시나요?", modificationTime: "2일 전 (수정됨)"),
+        Post(content: "좋은 치과 선생님 좀 소개해 주세요! 저희 아이가 12살인데 지금 교정을 하는 것이 맞을까요?", modificationTime: "2시간 전"),
+        Post(content: "교정 유지 장치가 갑자기 떨어져 버렸어요. 이거 어떻게 하는 게 좋을까요?", modificationTime: "10분 전")
+    ]
+    
+    @State private var selectedPost: Post?
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -76,11 +93,46 @@ struct CommunityMainView: View {
                 .padding(.horizontal)
                 .padding(.top, 15)
                 
+                VStack(alignment: .leading, spacing: 0) {
+                    ForEach(dummyPosts.prefix(4).indices, id: \.self) { index in
+                        postView(index: index)
+                            .padding(.top, 15)
+                            .padding(.bottom, 15)
+                    }
+                }
+                .padding(.horizontal)
+                .background(Color.white)
+                .cornerRadius(10)
+                .padding(.horizontal)
+                .shadow(color: Color.gray.opacity(0.5), radius: 3, x: 0, y: 1)
+                .padding(.top, 15)
                 Spacer()
             }
             .navigationBarTitle("", displayMode: .inline)
         }
         .accentColor(.gray)
+        
+    }
+    
+    func postView(index: Int) -> some View {
+        let post = dummyPosts[index]
+        return VStack(alignment: .leading, spacing: 5) {
+            HStack {
+                Text("\(index + 1). ")
+                    .font(.custom("Pretendard-Medium", size: 13))
+                    .foregroundColor(Color("BackgroundBlue"))
+                
+                Text("\(post.content)")
+                    .font(.custom("Pretendard-Medium", size: 13))
+                    .foregroundColor(Color("TextBlack"))
+                    .lineLimit(1)
+            }
+            
+            Text("\(post.modificationTime)")
+                .font(.custom("Pretendard-Regular", size: 10))
+                .foregroundColor(.gray)
+                .padding(.leading, 21)
+        }
     }
 }
 
