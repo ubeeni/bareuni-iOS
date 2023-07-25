a3b2b7f00dcc28c48d8efc71a22a56da02eb1518
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2023-07-25 20:11:58 +0900

    [Feat] 프로필 설정 - 성별, 나이 선택 페이지에서 버튼 이벤트 기능 추가

--- Bareuni/Controller/Profile/DetailedProfileSettingViewController.swift
+++ Bareuni/Controller/Profile/DetailedProfileSettingViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class DetailedProfileSettingViewController: UIViewController {
+    var sexBtnArr: [UIButton] = []
+    var ageBtnArr: [UIButton] = []
+    var toothBtnArr: [UIButton] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +18,10 @@ class DetailedProfileSettingViewController: UIViewController {
         self.setBackBtn()
         nextBtn.changeDisabledState()
         nextBtn.layer.cornerRadius = 12
+        
+        sexBtnArr = [womanBtn, manBtn]
+        ageBtnArr = [age1Btn, age2Btn, age3Btn, age4Btn, age5Btn, age6Btn]
+        toothBtnArr = [yesBtn, noBtn]
         
         setBtnDefaultBorder(manBtn); setBtnDefaultBorder(womanBtn); setBtnDefaultBorder(age1Btn); setBtnDefaultBorder(age2Btn); setBtnDefaultBorder(age3Btn); setBtnDefaultBorder(age4Btn); setBtnDefaultBorder(age5Btn); setBtnDefaultBorder(age6Btn)
         
@@ -39,38 +46,105 @@ class DetailedProfileSettingViewController: UIViewController {
         return age1Btn.isSelected || age2Btn.isSelected || age3Btn.isSelected || age4Btn.isSelected || age5Btn.isSelected || age6Btn.isSelected
     }
     
+    
+    
     @IBOutlet weak var yesBtn: UIButton!
     @IBOutlet weak var noBtn: UIButton!
-    var isReplyOneSelected: Bool {
+    var isToothOneSelected: Bool {
         return yesBtn.isSelected || noBtn.isSelected
     }
     
-    @IBAction func manBtnDidTap(_ sender: Any) {
-        if isSexOneSelected == false{
-            manBtn.isSelected = true
-            setBtnSelected(manBtn)
-        }
-        else{
-            manBtn.isSelected = true
-            setBtnSelected(manBtn)
-            
-            womanBtn.isSelected = true
-            setBtnDefaultBorder(womanBtn)
-        }
+    @IBAction func manBtnDidTap(_ sender: UIButton) {
+        setSexBtnSelected(sender, sender.isSelected)
     }
-    @IBAction func womanBtnDidTap(_ sender: Any) {
-        if isSexOneSelected == false{
-            womanBtn.isSelected = true
-            setBtnSelected(womanBtn)
-        }
-        else{
-            womanBtn.isSelected = true
-            setBtnSelected(womanBtn)
-            
-            manBtn.isSelected = true
-            setBtnDefaultBorder(manBtn)
-        }
+    @IBAction func womanBtnDidTap(_ sender: UIButton) {
+        setSexBtnSelected(sender, sender.isSelected)
     }
+    func setSexBtnSelected(_ clickedButton: UIButton, _ isSelected: Bool){ // 성별 버튼 처리 이벤트 함수
+        if(isSexOneSelected == false){
+            clickedButton.isSelected = true
+            setBtnSelected(clickedButton)
+        }else{
+            for i in sexBtnArr{
+                i.isSelected = false
+                setBtnDefaultBorder(i)
+            }
+            if(isSelected == false){
+                clickedButton.isSelected = true
+                setBtnSelected(clickedButton)
+            }else{
+                setBtnDefaultBorder(clickedButton)
+            }
+        }
+        isConfirmBtnEnabled()
+    }
+    
+    @IBAction func age10BtnDidTap(_ sender: UIButton) {
+        setAgeBtnSelected(sender, sender.isSelected)
+    }
+    @IBAction func age20BtnDidTap(_ sender: UIButton) {
+        setAgeBtnSelected(sender, sender.isSelected)
+    }
+    
+    @IBAction func age30BtnDidTap(_ sender: UIButton) {
+        setAgeBtnSelected(sender, sender.isSelected)
+    }
+    @IBAction func age40BtnDidTap(_ sender: UIButton) {
+        setAgeBtnSelected(sender, sender.isSelected)
+    }
+    @IBAction func age50BtnDidTap(_ sender: UIButton) {
+        setAgeBtnSelected(sender, sender.isSelected)
+    }
+    @IBAction func age60BtnDidTap(_ sender: UIButton) {
+        setAgeBtnSelected(sender, sender.isSelected)
+    }
+    
+    func setAgeBtnSelected(_ clickedButton: UIButton, _ isSelected: Bool){ // 나이 클릭 이벤트 처리 함수
+        if(isAgeOneSelected == false){
+            clickedButton.isSelected = true
+            setBtnSelected(clickedButton)
+        }else{
+            for i in ageBtnArr{
+                i.isSelected = false
+                setBtnDefaultBorder(i)
+            }
+            if(isSelected == false){
+                clickedButton.isSelected = true
+                setBtnSelected(clickedButton)
+            }else{
+                setBtnDefaultBorder(clickedButton)
+            }
+        }
+        isConfirmBtnEnabled()
+    }
+    
+    @IBAction func yesBtnDidTap(_ sender: UIButton) {
+        setToothBtnSelected(sender, sender.isSelected)
+    }
+    
+    @IBAction func noBtnDidTap(_ sender: UIButton) {
+        setToothBtnSelected(sender, sender.isSelected)
+    }
+    
+    func setToothBtnSelected(_ clickedButton: UIButton, _ isSelected: Bool){ // 교정여부 클릭 이벤트 처리 함수
+        if(isToothOneSelected == false){
+            clickedButton.isSelected = true
+            setBtnSelected(clickedButton)
+        }else{
+            for i in toothBtnArr{
+                i.isSelected = false
+                setBtnDefaultBorder(i)
+            }
+            if(isSelected == false){
+                clickedButton.isSelected = true
+                setBtnSelected(clickedButton)
+            }else{
+                setBtnDefaultBorder(clickedButton)
+            }
+        }
+        isConfirmBtnEnabled()
+    }
+    
     
     func setBtnDefaultBorder(_ button: UIButton){
         button.setTitleColor(UIColor(red: 0.729, green: 0.729, blue: 0.729, alpha: 1), for: .normal)
@@ -80,6 +154,17 @@ class DetailedProfileSettingViewController: UIViewController {
     }
     
     func setBtnSelected(_ button: UIButton){
-        button.setTitleColor(UIColor(red: 0, green: 0.58, blue: 1, alpha: 1), for: .normal)
+        button.setTitleColor(UIColor(red: 1, green: 0.58, blue: 1, alpha: 1), for: .normal)
+    }
+    func isConfirmBtnEnabled(){
+        if isSexOneSelected && isAgeOneSelected && isToothOneSelected {
+            nextBtn.changeEnabledState()
+        }
+        else{
+            nextBtn.changeDisabledState()
+        }
+    }
+    @IBAction func confirmBtnDidTap(_ sender: Any) {
+       
     }
 }
