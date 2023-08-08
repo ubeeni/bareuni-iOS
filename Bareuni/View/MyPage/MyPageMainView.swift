a2e8ce943fb7aab160c3ddc38f1cee1dab53ae7a
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2023-08-08 20:54:31 +0900

    [Feat] 마이페이지 swiftUI 세팅

---
+++ Bareuni/View/MyPage/MyPageMainView.swift
@@ -0,0 +1,23 @@
+//
+//  MyPageMainView.swift
+//  Bareuni
+//
+//  Created by 윤지성 on 2023/08/08.
+//
+
+import SwiftUI
+
+struct MyPageMainView: View {
+  var body: some View {
+    VStack {
+      Text("SwiftUI 뷰")
+      Text("Hello, World!")
+    }
+  }
+}
+
+struct MyPageMainView_Previews: PreviewProvider {
+  static var previews: some View {
+      MyPageMainView()
+  }
+}
