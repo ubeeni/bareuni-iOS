0a9bc274d2d3577d70741056e9d399ae6acf14d6
green-yoon87 <113659520+green-yoon87@users.noreply.github.com>
2023-08-09 14:02:55 +0900

    [Feat] 닉네임/이름 변경 ui 버튼, 텍스트 필드 이벤트, 경고 라벨 이벤트 추가

--- Bareuni/View/MyPage/ChangingNicknameView.swift
+++ Bareuni/View/MyPage/ChangingNicknameView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct ChangingNicknameView: View {
+    @State var bfNickname: String = "이아파"
+    @State var bfName: String = "김민지"
     @State var nickname: String = "이아파"
     @State var name: String = "김민지"
     @State var selectedCities: Int = 0
+    @State private var isNickNameExisted = false
+    
     @Environment(\.dismiss) var dismiss
     
     var body: some View {
-        VStack{
+        VStack(alignment: .leading){
             HStack{
                 Spacer()
                 Text("닉네임 / 이름 변경").font(.custom("Pretendard-Medium", size: 20)).padding(.leading, 24)
@@ -30,6 +34,7 @@ struct ChangingNicknameView: View {
             VStack(alignment: .leading){
                 Text("닉네임").font(.custom("Pretendard-Medium", size: 16)).padding(.leading, 8)
                 
+                
                 TextField("이아파", text: $nickname)
                     .padding(16)
                     .foregroundColor(Color(UIColor(red: 0.38, green: 0.38, blue: 0.38, alpha: 1)))
@@ -39,6 +44,10 @@ struct ChangingNicknameView: View {
                     ).frame(height: 48).padding(.top, 8)
                 
             }.padding(.trailing, 24).padding(.leading, 24).padding(.top, 35)
+            
+            
+            Text("중복된 닉네임 입니다. ").font(.custom("Pretendard-Regular", size: 12)).padding(.leading, 8).foregroundColor(Color(UIColor(red: 1, green: 0.314, blue: 0.314, alpha: 1))).frame(height: 30).padding(.leading, 35).opacity(isNickNameExisted ? 1 : 0)
+            
             
             VStack(alignment: .leading){
                 Text("이름").font(.custom("Pretendard-Medium", size: 16)).padding(.leading, 8)
@@ -50,34 +59,34 @@ struct ChangingNicknameView: View {
                             .stroke(Color(UIColor(red: 0.878, green: 0.878, blue: 0.878, alpha: 1)), lineWidth: 0.5)
                     ).frame(height: 48).padding(.top, 8)
                 
-            }.padding(.trailing, 24).padding(.leading, 24).padding(.top, 27)
+            }.padding(.trailing, 24).padding(.leading, 24)
             
             
             Button(action: {
                 //아마 API연결
             }, label: {
                 NavigationLink(destination: InfoView(), label: {
-                ZStack {
-                    
-                    if selectedCities == 0 {
-                        Rectangle().frame( height: 51)
-                            .cornerRadius(4)
-                            .foregroundColor(Color("disabledBtnColor"))
+                    ZStack {
+                        
+                        if bfNickname == nickname && bfName == name {
+                            Rectangle().frame( height: 51)
+                                .cornerRadius(4)
+                                .foregroundColor(Color("disabledBtnColor"))
+                        }
+                        else{
+                            Rectangle().frame( height: 51)
+                                .cornerRadius(4)
+                                .foregroundColor(Color("BackgroundBlue"))
+                        }
+                        
+                        Text("설정 완료")
+                            .font(
+                                Font.custom("Pretendard-SemiBold", size: 16)
+                                    .weight(.bold)
+                            )
+                            .multilineTextAlignment(.trailing)
+                            .foregroundColor(.white)
                     }
-                    else{
-                        Rectangle().frame( height: 51)
-                            .cornerRadius(4)
-                            .foregroundColor(Color("BackgroundBlue"))
-                    }
-                    
-                    Text("설정 완료")
-                        .font(
-                            Font.custom("Pretendard-SemiBold", size: 16)
-                                .weight(.bold)
-                        )
-                        .multilineTextAlignment(.trailing)
-                        .foregroundColor(.white)
-                }
                 })
             }).padding(.top, 34).padding(.trailing, 24).padding(.leading, 24)
             
