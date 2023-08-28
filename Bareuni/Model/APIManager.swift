45e030c9163417ab52802cd049f070aa860a4311
김유빈 <yubin6894@gmail.com>
2023-08-28 17:56:46 +0900

    [Docs] APIManager 파일 생성

---
+++ Bareuni/Model/APIManager.swift
@@ -0,0 +1,61 @@
+//
+//  APIManager.swift
+//  Bareuni
+//
+//  Created by 김유빈 on 2023/08/28.
+//
+
+import Foundation
+import Moya
+
+enum BareuniAPI {
+    case getBestDentist
+    case getBestCommunity
+}
+
+extension BareuniAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: "http://54.180.54.10:8080/")!
+    }
+    
+    var path: String {
+        switch self {
+        case .getBestDentist:
+            return "hospital/best"
+        case .getBestCommunity:
+            return "community/best"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self{
+        case .getBestDentist:
+            return .get
+        case .getBestCommunity:
+            return .get
+        }
+    }
+    
+    var sampleData: Data {
+        switch self{
+        case .getBestDentist:
+            return Data()
+        case .getBestCommunity:
+            return Data()
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .getBestDentist:
+            return .requestPlain
+        case .getBestCommunity:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String: String]? {
+        return nil
+    }
+    
+}
